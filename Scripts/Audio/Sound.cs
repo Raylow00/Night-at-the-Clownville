49af414b322496baba84192f52c1b3de8d7bc52e
Ray Low <raychunyin00@gmail.com>
2022-12-26 11:17:53 +0800

    NATCV-63: Added public methods for Sound to Init, Play and Stop sound and to get audio name. Added enum for mixer group types. Added private methods to init each setting of a sound

--- Scripts/Audio/Sound.cs
+++ Scripts/Audio/Sound.cs
@@ -1,63 +1,178 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/// <summary>
+///     Enumeration for the types of audio mixer group used
+/// </summary>
+enum MixerGroup
+{
+    MASTER,
+    MUSIC,
+    SFX,
+    AMBIENCE
+}
+
+/// <summary>
+///     Sound class that encompasses properties of a sound
+/// </summary>
 [System.Serializable]
 public class Sound
 {
-    #region Fields
-    [SerializeField] private string strAudioName;
+    #region Private Fields
+    [SerializeField] private string audioName;
 
-    [SerializeField] private AudioSource audioSource_AudioSource;
-    [SerializeField] private AudioMixerGroup audioMixerGroup_AudioMixerGroup;
-    [SerializeField] private AudioClip[] audioClip_AudioClips;
+    [SerializeField] private AudioSettingsScriptableObject audioSettingsSO;
+    [SerializeField] private MixerGroup mixerGroup;
+    private AudioSource audioSource;
+    [SerializeField] private AudioMixerGroup audioMixerGroup;
+    [SerializeField] private AudioClip[] audioClips;
+    
+    [Range(0f, 1.01f)]
+    [Tooltip("Setting audio volume to 1.01f forces it to use the volume from AudioSettingsSO")]
+    [SerializeField] private float audioVolume;
 
-    [Range(0f, 1f)]
-    [SerializeField] private float f8audioVolume;
+    [Range(0.1f, 1f)]
+    [SerializeField] private float audioPitch;
 
-    [Range(0.1f, 3f)]
-    [SerializeField] private float f8audioPitch;
+    [SerializeField] private bool toPlayRandomClip;
+    [SerializeField] private bool isAudioLooping;
+    [SerializeField] private bool toPlayOnAwake;
+    #endregion
 
-    [SerializeField] private bool boIsAudioLooping;
-    [SerializeField] private bool boToPlayOnAwake;
+    #region Constructor
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    /// <param name="arg_audioSource">Audio source created at runtime for each sound</param>
+    public Sound(AudioSource arg_audioSource)
+    {
+        InitAudioSource(arg_audioSource);
+        InitAudioVolume();
+        InitAudioPitch();
+        InitAudioLooping();
+        InitAudioMixerGroup();
+    }
+    #endregion
+
+    #region Properties
+    /// <summary>
+    ///     Get audio name
+    /// </summary>
+    /// <returns>audioName</returns>
+    public string GetAudioName()
+    {
+        return audioName;
+    }
     #endregion
 
     #region Public Methods
-    public void vInitSound(AudioSource audioSource)
+    /// <summary>
+    ///     Initialize each sound with their respective settings in the Awake function
+    ///     in volume, pitch, audio mixer group and whether it is looping
+    ///     If toPlayOnAwake is checked, play sound
+    /// </summary>
+    /// <param name="arg_audioSource"></param>
+    public void InitSound(AudioSource arg_audioSource)
     {
-        vSetAudioSource(audioSource);
-        vSetAudioVolume();
-        vSetAudioPitch();
-        vSetAudioLooping();
-        vSetAudioMixerGroup();
+        InitAudioSource(arg_audioSource);
+        InitAudioVolume();
+        InitAudioPitch();
+        InitAudioLooping();
+        InitAudioMixerGroup();
+
+        if (toPlayOnAwake) PlaySound();
+    }
+
+    /// <summary>
+    ///     Play sound
+    /// </summary>
+    public void PlaySound()
+    {
+        // Set audio clip
+        int clipIndex = 0;
+        if (toPlayRandomClip)
+        {
+            clipIndex = UnityEngine.Random.Range(0, audioClips.Length);
+        }
+
+        audioSource.clip = audioClips[clipIndex];
+
+        // Set audio volume
+        if (audioVolume < 1.01f)
+        {
+            audioSource.volume = audioVolume;
+        }
+
+        audioSource.Play();
+    }
+
+    /// <summary>
+    ///     Stop playing sound
+    /// </summary>
+    public void StopSound()
+    {
+        audioSource.Stop();
     }
     #endregion
 
     #region Private Methods
-    private void vSetAudioSource(AudioSource audioSource)
+    /// <summary>
+    ///     Sets the audio source of a sound to the source passed in
+    /// </summary>
+    /// <param name="arg_audioSource"></param>
+    private void InitAudioSource(AudioSource arg_audioSource)
     {
-        audioSource_AudioSource = audioSource;
+        audioSource = arg_audioSource;
     }
 
-    private void vSetAudioVolume()
+    /// <summary>
+    ///     Sets the mixer group to the mixer group assigned to this sound
+    /// </summary>
+    private void InitAudioMixerGroup()
     {
-        audioSource_AudioSource.volume = f8audioVolume;
+        audioSource.outputAudioMixerGroup = audioMixerGroup;
     }
 
-    private void vSetAudioPitch()
+    /// <summary>
+    ///     Initializes the audio volume based on the mixer group type selected
+    ///     Defaults to master volume
+    /// </summary>
+    private void InitAudioVolume()
     {
-        audioSource_AudioSource.pitch = f8audioPitch;
+        switch (mixerGroup)
+        {
+            case MixerGroup.MASTER:
+                audioSource.volume = audioSettingsSO.masterVolume;
+                break;
+            case MixerGroup.MUSIC:
+                audioSource.volume = audioSettingsSO.musicVolume;
+                break;
+            case MixerGroup.SFX:
+                audioSource.volume = audioSettingsSO.sfxVolume;
+                break;
+            case MixerGroup.AMBIENCE:
+                audioSource.volume = audioSettingsSO.ambienceVolume;
+                break;
+            default:
+                audioSource.volume = audioSettingsSO.masterVolume;
+                break;
+        }
     }
 
-    private void vSetAudioLooping()
+    /// <summary>
+    ///     Initializes audio pitch to the value assigned to this sound
+    /// </summary>
+    private void InitAudioPitch()
     {
-        audioSource_AudioSource.loop = boIsAudioLooping;
+        audioSource.pitch = audioPitch;
     }
 
-    private void vSetAudioMixerGroup()
+    /// <summary>
+    ///     Sets whether this sound is looping
+    /// </summary>
+    private void InitAudioLooping()
     {
-        audioSource_AudioSource.outputAudioMixerGroup = audioMixerGroup_AudioMixerGroup;
+        audioSource.loop = isAudioLooping;
     }
     #endregion
 }

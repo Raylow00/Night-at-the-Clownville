2a9b64118a34febf566f42e08c074a6500e18efd
Ray Low <raychunyin00@gmail.com>
2022-12-26 11:19:26 +0800

    NATCV-63: Added sound init function to Awake function. Added public methods to play and stop audio.

--- Scripts/Audio/AudioManager.cs
+++ Scripts/Audio/AudioManager.cs
@@ -1,19 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+/// <summary>
+///     Audio manager class that manages/plays/stops audio
+/// </summary>
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] private string strAudioManagerName;
-    [SerializeField] private Sound[] sound_Sounds;
-    [SerializeField] private SO_AudioSettings so_AudioSettings;
+    #region Private Fields
+    [SerializeField] private string audioManagerName;
+    [SerializeField] private Sound[] sounds;
+    #endregion
 
+    /// <summary>
+    ///     Initializes all sounds
+    ///     To play sound if toPlayOnAwake in sound is checked
+    /// </summary>
     void Awake()
     {
-        foreach (Sound s in sound_Sounds)
+        foreach (Sound sound in sounds)
         {
             AudioSource source = gameObject.AddComponent<AudioSource>();
-            s.vInitSound(source);
+            sound.InitSound(source);
         }
     }
+
+    #region Public Methods
+    /// <summary>
+    ///     Finds the sound in the array and play audio
+    /// </summary>
+    /// <param name="arg_audioName"></param>
+    public void PlayAudio(string arg_audioName)
+    {
+        Sound s = Array.Find(sounds, sound => sound.GetAudioName() == arg_audioName);
+        if (s == null)
+        {
+            return;
+        }
+        else
+        {
+            s.PlaySound();
+        }
+    }
+
+    /// <summary>
+    ///     Finds the sound in the array and stop audio
+    /// </summary>
+    /// <param name="arg_audioName"></param>
+    public void StopAudio(string arg_audioName)
+    {
+        Sound s = Array.Find(sounds, sound => sound.GetAudioName() == arg_audioName);
+        if (s == null)
+        {
+            return;
+        }
+        else
+        {
+            s.StopSound();
+        }
+    }
+    #endregion
 }

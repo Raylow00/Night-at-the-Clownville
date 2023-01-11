e938b6379a2b5355d2e5d266792be725d631600b
Ray Low <raychunyin00@gmail.com>
2023-01-11 23:28:28 +0800

    NATCV-120: Added code to bind action keys and to rebind them, proposing two methods, commented out one, using one. To add mouse sensitivity and field of view functionalities.

---
+++ Scripts/Utility/GameplaySettingsManager.cs
@@ -0,0 +1,374 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameplaySettingsManager : MonoBehaviour
+{
+    #region Serialized Fields
+    [SerializeField] private GameplaySettingsScriptableObject gameplaySettingsSO;
+    #endregion
+
+    #region Private Fields
+    private PlayerController playerController;
+    private PlayerController.GameplayControlsActions gameplayControlsActions;
+    private InputAction reloadAction;
+    private InputAction crouchAction;
+    private InputAction flashlightAction;
+    private InputAction switchLastUsedWeaponAction;
+    private InputAction equipCameraAction;
+    private InputAction viewMapAction;
+    //private InputAction pauseAction;
+
+    private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
+    private static GameplaySettingsManager instance;
+    #endregion
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(this);
+
+        playerController = new PlayerController();
+        gameplayControlsActions = playerController.GameplayControls;
+        reloadAction = gameplayControlsActions.Reload;
+        crouchAction = gameplayControlsActions.Crouch;
+        flashlightAction = gameplayControlsActions.Flashlight;
+        switchLastUsedWeaponAction = gameplayControlsActions.SwitchEquipment;
+        equipCameraAction = gameplayControlsActions.Camera;
+        viewMapAction = gameplayControlsActions.UseMap;
+
+        InitDefaultGameplaySettings();
+        AssignActionsToKeys();
+    }
+
+    void OnEnable()
+    {
+        playerController.Enable();
+    }
+
+    void OnDestroy()
+    {
+        playerController.Disable();
+    }
+
+    #region Public Methods
+    /// <summary>
+    ///     Get the scriptable object GameplaySettingsScriptableObject
+    /// </summary>
+    /// <returns>
+    ///     GameplaySettingsScriptableObject
+    /// </returns>
+    public GameplaySettingsScriptableObject GetScriptableObject()
+    {
+        return gameplaySettingsSO;
+    }
+
+    /// <summary>
+    ///     Remaps the reload key by sending the old key + new key
+    /// </summary>
+    /// <param name="arg_newKey"></param>
+    public void RemapReloadKey(string arg_newKey)
+    {
+        string keyData = gameplaySettingsSO.reloadKey + arg_newKey;
+        OverrideKeyBindings(keyData);
+    }
+
+    /// <summary>
+    ///     Remaps the crouch key by sending the old key + new key
+    /// </summary>
+    /// <param name="arg_newKey"></param>
+    public void RemapCrouchKey(string arg_newKey)
+    {
+        string keyData = gameplaySettingsSO.crouchKey + arg_newKey;
+        OverrideKeyBindings(keyData);
+    }
+
+    /// <summary>
+    ///     Remaps the flashlight key by sending the old key + new key
+    /// </summary>
+    /// <param name="arg_newKey"></param>
+    public void RemapFlashlightKey(string arg_newKey)
+    {
+        string keyData = gameplaySettingsSO.flashlightKey + arg_newKey;
+        OverrideKeyBindings(keyData);
+    }
+
+    /// <summary>
+    ///     Remaps the switch last used weapon key by sending the old key + new key
+    /// </summary>
+    /// <param name="arg_newKey"></param>
+    public void RemapSwitchLastUsedWeaponKey(string arg_newKey)
+    {
+        string keyData = gameplaySettingsSO.switchLastUsedWeaponKey + arg_newKey;
+        OverrideKeyBindings(keyData);
+    }
+
+    /// <summary>
+    ///     Remaps the view map key by sending the old key + new key
+    /// </summary>
+    /// <param name="arg_newKey"></param>
+    public void RemapViewMapKey(string arg_newKey)
+    {
+        string keyData = gameplaySettingsSO.viewMapKey + arg_newKey;
+        OverrideKeyBindings(keyData);
+    }
+
+    /// <summary>
+    ///     Remaps the equip camera key by sending the old key + new key
+    /// </summary>
+    /// <param name="arg_newKey"></param>
+    public void RemapEquipCameraKey(string arg_newKey)
+    {
+        string keyData = gameplaySettingsSO.equipCameraKey + arg_newKey;
+        OverrideKeyBindings(keyData);
+    }
+    #endregion
+
+    #region Private Methods
+    /// <summary>
+    ///     Initialize all keys to the default values in the GameplaySettingsScriptableObject
+    /// </summary>
+    private void InitDefaultGameplaySettings()
+    {
+        gameplaySettingsSO.reloadKey = gameplaySettingsSO.default_ReloadKey;
+        gameplaySettingsSO.crouchKey = gameplaySettingsSO.default_CrouchKey;
+        gameplaySettingsSO.flashlightKey = gameplaySettingsSO.default_FlashlightKey;
+        gameplaySettingsSO.switchLastUsedWeaponKey = gameplaySettingsSO.default_SwitchLastUsedWeaponKey;
+        gameplaySettingsSO.viewMapKey = gameplaySettingsSO.default_ViewMapKey;
+        gameplaySettingsSO.equipCameraKey = gameplaySettingsSO.default_EquipCameraKey;
+
+        gameplaySettingsSO.mouseSensitivityX = gameplaySettingsSO.default_MouseSensitivityX;
+        gameplaySettingsSO.mouseSensitivityY = gameplaySettingsSO.default_MouseSensitivityY;
+
+        gameplaySettingsSO.fieldOfView = gameplaySettingsSO.default_FieldOfView;
+
+        // Reload key
+        InputBinding reloadBinding = reloadAction.bindings[0];
+        reloadBinding.overridePath = "<Keyboard>/#(" + gameplaySettingsSO.reloadKey + ")";
+        reloadAction.ApplyBindingOverride(0, reloadBinding);
+
+        // Crouch key
+        InputBinding crouchBinding = crouchAction.bindings[0];
+        crouchBinding.overridePath = "<Keyboard>/#(" + gameplaySettingsSO.crouchKey + ")";
+        crouchAction.ApplyBindingOverride(0, crouchBinding);
+
+        // Flashlight key
+        InputBinding flashlightBinding = flashlightAction.bindings[0];
+        flashlightBinding.overridePath = "<Keyboard>/#(" + gameplaySettingsSO.flashlightKey + ")";
+        flashlightAction.ApplyBindingOverride(0, flashlightBinding);
+
+        // Switch last used weapon key
+        InputBinding lastUsedWeaponBinding = switchLastUsedWeaponAction.bindings[0];
+        lastUsedWeaponBinding.overridePath = "<Keyboard>/#(" + gameplaySettingsSO.switchLastUsedWeaponKey + ")";
+        switchLastUsedWeaponAction.ApplyBindingOverride(0, lastUsedWeaponBinding);
+
+        // View map key
+        InputBinding mapBinding = viewMapAction.bindings[0];
+        mapBinding.overridePath = "<Keyboard>/#(" + gameplaySettingsSO.viewMapKey + ")";
+        viewMapAction.ApplyBindingOverride(0, mapBinding);
+
+        // Equip camera key
+        InputBinding cameraBinding = equipCameraAction.bindings[0];
+        cameraBinding.overridePath = "<Keyboard>/#(" + gameplaySettingsSO.equipCameraKey + ")";
+        equipCameraAction.ApplyBindingOverride(0, cameraBinding);
+    }
+
+    /// <summary>
+    ///     Assign each function to each action
+    /// </summary>
+    private void AssignActionsToKeys()
+    {
+        gameplayControlsActions.Reload.performed += _ => OnReloadPressed();
+        gameplayControlsActions.Crouch.performed += _ => OnCrouchPressed();
+        gameplayControlsActions.Flashlight.performed += _ => OnFlashlightPressed();
+        gameplayControlsActions.SwitchEquipment.performed += _ => OnSwitchLastUsedWeaponPressed();
+        gameplayControlsActions.Camera.performed += _ => OnEquipCameraPressed();
+        gameplayControlsActions.UseMap.performed += _ => OnUseMapPressed();
+    }
+
+    /// <summary>
+    ///     Handles the rebinding by setting the override path of each action binding
+    ///     The commented out code refers to the interactive binding during runtime.
+    ///     It is commented out and not used so that the input from players can be taken from the UI input field 
+    ///     instead of the real-time input.
+    /// </summary>
+    /// <param name="arg_keyData"></param>
+    private void OverrideKeyBindings(string arg_keyData)
+    {
+        // Input will be the original character followed by character to switch to
+        // For example, "CF"
+        // "C" - original key for crouching
+        // "F" - key to switch to for crouching
+        string originalKey = arg_keyData.Substring(0, 1);
+        string newKey = arg_keyData.Substring(1, 1);
+
+        switch (originalKey)
+        {
+            case "r":
+                reloadAction.Disable();
+                //rebindingOperation = reloadAction.PerformInteractiveRebinding()
+                //                        .WithControlsExcluding("Mouse")
+                //                        .OnMatchWaitForAnother(0.1f)
+                //                        .OnComplete(operation => OnRebindComplete("Reload", reloadAction, rebindingOperation))
+                //                        .Start();
+                InputBinding reloadBinding = reloadAction.bindings[0];
+                reloadBinding.overridePath = "<Keyboard>/#(" + newKey + ")";
+                reloadAction.ApplyBindingOverride(0, reloadBinding);
+                reloadAction.Enable();
+
+                gameplaySettingsSO.reloadKey = newKey;
+
+                break;
+            case "c":
+                crouchAction.Disable();
+                //rebindingOperation = crouchAction.PerformInteractiveRebinding()
+                //                        .WithControlsExcluding("Mouse")
+                //                        .WithControlsExcluding("<Keyboard>/Enter")
+                //                        .WithControlsExcluding("<Keyboard>/numpadEnter")
+                //                        .OnMatchWaitForAnother(0.1f)
+                //                        .OnComplete(operation => OnRebindComplete("Crouch", crouchAction, rebindingOperation))
+                //                        .Start();
+                InputBinding crouchBinding = crouchAction.bindings[0];
+                crouchBinding.overridePath = "<Keyboard>/#(" + newKey + ")";
+                crouchAction.ApplyBindingOverride(0, crouchBinding);
+                crouchAction.Enable();
+
+                gameplaySettingsSO.crouchKey = newKey;
+
+                break;
+            case "f":
+                flashlightAction.Disable();
+                //rebindingOperation = flashlightAction.PerformInteractiveRebinding()
+                //                        .WithControlsExcluding("Mouse")
+                //                        .OnMatchWaitForAnother(0.1f)
+                //                        .OnComplete(operation => OnRebindComplete("Flashlight", flashlightAction, rebindingOperation))
+                //                        .Start();
+                InputBinding flashlightBinding = flashlightAction.bindings[0];
+                flashlightBinding.overridePath = "<Keyboard>/#(" + newKey + ")";
+                flashlightAction.ApplyBindingOverride(0, flashlightBinding);
+                flashlightAction.Enable();
+
+                gameplaySettingsSO.flashlightKey = newKey;
+
+                break;
+            case "q":
+                switchLastUsedWeaponAction.Disable();
+                //rebindingOperation = switchLastUsedWeaponAction.PerformInteractiveRebinding()
+                //                        .WithControlsExcluding("Mouse")
+                //                        .OnMatchWaitForAnother(0.1f)
+                //                        .OnComplete(operation => OnRebindComplete("SwitchLastUsedWeapon", switchLastUsedWeaponAction, rebindingOperation))
+                //                        .Start();
+                InputBinding lastUsedWeaponBinding = switchLastUsedWeaponAction.bindings[0];
+                lastUsedWeaponBinding.overridePath = "<Keyboard>/#(" + newKey + ")";
+                switchLastUsedWeaponAction.ApplyBindingOverride(0, lastUsedWeaponBinding);
+                switchLastUsedWeaponAction.Enable();
+
+                gameplaySettingsSO.switchLastUsedWeaponKey = newKey;
+
+                break;
+            case "m":
+                viewMapAction.Disable();
+                //rebindingOperation = viewMapAction.PerformInteractiveRebinding()
+                //                        .WithControlsExcluding("Mouse")
+                //                        .OnMatchWaitForAnother(0.1f)
+                //                        .OnComplete(operation => OnRebindComplete("ViewMap", viewMapAction, rebindingOperation))
+                //                        .Start();
+                InputBinding mapBinding = viewMapAction.bindings[0];
+                mapBinding.overridePath = "<Keyboard>/#(" + newKey + ")";
+                viewMapAction.ApplyBindingOverride(0, mapBinding);
+                viewMapAction.Enable();
+
+                gameplaySettingsSO.viewMapKey = newKey;
+
+                break;
+            case "g":
+                equipCameraAction.Disable();
+                //rebindingOperation = equipCameraAction.PerformInteractiveRebinding()
+                //                        .WithControlsExcluding("Mouse")
+                //                        .OnMatchWaitForAnother(0.1f)
+                //                        .OnComplete(operation => OnRebindComplete("EquipCamera", equipCameraAction, rebindingOperation))
+                //                        .Start();
+                InputBinding cameraBinding = equipCameraAction.bindings[0];
+                cameraBinding.overridePath = "<Keyboard>/#(" + newKey + ")";
+                equipCameraAction.ApplyBindingOverride(0, cameraBinding);
+                equipCameraAction.Enable();
+
+                gameplaySettingsSO.equipCameraKey = newKey;
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    ///     Action to perform when reload is pressed
+    /// </summary>
+    private void OnReloadPressed()
+    {
+        Debug.Log("Reload key pressed!");
+    }
+
+    /// <summary>
+    ///     Action to perform when use map is pressed
+    /// </summary>
+    private void OnUseMapPressed()
+    {
+        Debug.Log("Map key pressed!");
+    }
+
+    /// <summary>
+    ///     Action to perform when equip camera is pressed
+    /// </summary>
+    private void OnEquipCameraPressed()
+    {
+        Debug.Log("Camera key pressed!");
+    }
+
+    /// <summary>
+    ///     Action to perform when switch last used weapon is pressed
+    /// </summary>
+    private void OnSwitchLastUsedWeaponPressed()
+    {
+        Debug.Log("Switch last used weapon key pressed!");
+    }
+
+    /// <summary>
+    ///     Action to perform when crouch is pressed
+    /// </summary>
+    private void OnCrouchPressed()
+    {
+        Debug.Log("Crouch key pressed!");
+    }
+
+    /// <summary>
+    ///     Action to perform when flashlight is pressed
+    /// </summary>
+    private void OnFlashlightPressed()
+    {
+        Debug.Log("Flashlight key pressed!");
+    }
+
+    /// <summary>
+    ///     Unused callback function for dynamic rebinding of keys 
+    ///     Referenced in function OverrideKeyBindings()
+    /// </summary>
+    /// <param name="keyAction"></param>
+    /// <param name="inputAction"></param>
+    /// <param name="operation"></param>
+    private void OnRebindComplete(string keyAction, InputAction inputAction, InputActionRebindingExtensions.RebindingOperation operation)
+    {
+        operation.Dispose();
+        inputAction.Enable();;
+    }
+    #endregion
+}

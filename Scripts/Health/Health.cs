983e95d04580afbd3a424a079930061fa321dabc
Ray Low <raychunyin00@gmail.com>
2023-01-29 13:01:08 +0800

    NATCV-183: Renamed EnemyHealth to Health, any entity or game object is able to use this Health class where necessary

--- Scripts/Health/Health.cs
+++ Scripts/Health/Health.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
+public class Health : MonoBehaviour
 {
     #region Serialized Fields
     [SerializeField] private TextViewer textViewer;
